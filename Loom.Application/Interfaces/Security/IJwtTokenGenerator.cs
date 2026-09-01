ae92cb12af3f20f83701d0c0ddfc91226eba6202
LaGretta <sashagutsul2014@gmail.com>
2026-09-01 23:08:29 +0200

    Add Security Interfaces

---
+++ Loom.Application/Interfaces/Security/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Loom.Domain.Entities;
+
+namespace Loom.Application.Interfaces.Security;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateJwtToken(User user);
+    string GenerateRefreshToken();
+}
---
+++ Loom.Application/Interfaces/Security/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace Loom.Application.Interfaces.Security;
+
+public interface IPasswordHasher
+{
+    string Hash(string password);
+    bool Verify(string password, string passwordHash);
+}
